36da0aa17b36a9684b4b7876934a2c152432fd60
mintaeKim <peterzzo@naver.com>
2024-03-27 00:11:54 +0900

    create account email add

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,111 +1,128 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../firebase";
+
 
 const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 420px;
+    padding: 50px 0px;
 `;
 
 const Title = styled.h1`
-  font-size: 42px;
+    font-size: 42px;
 `;
 
 const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
 `;
 
 const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
+    padding: 10px 20px;
+    border-radius: 50px;
+    border: none;
+    width: 100%;
+    font-size: 16px;
+    &[type="submit"] {
+        cursor: pointer;
+        &:hover {
+        opacity: 0.8;
+        }
     }
-  }
 `;
 
 const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
+    font-weight: 600;
+    color: tomato;
 `;
 
 export default function CreateAccount() {
-  const [isLoading, setLoading] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => { //form 의 name, value 를 가져와서
-    const {
-      target: { name, value },
-    } = e;
-    if (name === "name") {
-      setName(value);
-    } else if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };//name, email, password 에 넣어준다
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {
+        target: { name, value },
+        } = e;
+        if (name === "name") {
+        setName(value);
+        } else if (name === "email") {
+        setEmail(value);
+        } else if (name === "password") {
+        setPassword(value);
+        }
+    };//form 의 name, value 를 가져와서 name, email, password 에 넣어준다
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      // create an account
-      // set the name of the user.
-      // redirect to the home page
-    } catch (e) {
-      // setError
-    } finally {
-      setLoading(false);
-    }
-  };
-  return (
-    <Wrapper>
-        <Title>Log into 𝕏</Title>
-        <Form onSubmit={onSubmit}>
-            <Input
-                onChange={onChange}
-                name="name"
-                value={name}
-                placeholder="Name"
-                type="text"
-                required
-            />
-            <Input
-                onChange={onChange}
-                name="email"
-                value={email}
-                placeholder="Email"
-                type="email"
-                required
-            />
-            <Input
-                onChange={onChange}
-                value={password}
-                name="password"
-                placeholder="Password"
-                type="password"
-                required
-            />
-            <Input
-                type="submit"
-                value={isLoading ? "Loading..." : "Create Account"}
-            />
-        </Form>
-        {error !== "" ? <Error>{error}</Error> : null}
-    </Wrapper>
-  );
+    const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (isLoading || name === "" || email === "" || password === "") return;
+        try {
+            setLoading(true);
+            const credentials = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            console.log(credentials.user);
+            await updateProfile(credentials.user, {
+                displayName: name,
+            });
+            navigate("/");
+            // create an account
+            // set the name of the user.
+            // redirect to the home page
+        } catch (e) {
+        // setError
+        } finally {
+        setLoading(false);
+        }
+    };
+    return (
+        <Wrapper>
+            <Title>Join 𝕏</Title>
+            <Form onSubmit={onSubmit}>
+                <Input
+                    onChange={onChange}
+                    name="name"
+                    value={name}
+                    placeholder="Name"
+                    type="text"
+                    required
+                />
+                <Input
+                    onChange={onChange}
+                    name="email"
+                    value={email}
+                    placeholder="Email"
+                    type="email"
+                    required
+                />
+                <Input
+                    onChange={onChange}
+                    value={password}
+                    name="password"
+                    placeholder="Password"
+                    type="password"
+                    required
+                />
+                <Input
+                    type="submit"
+                    value={isLoading ? "Loading..." : "Create Account"}
+                />
+            </Form>
+            {error !== "" ? <Error>{error}</Error> : null}
+        </Wrapper>
+    );
 }
